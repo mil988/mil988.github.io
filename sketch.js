bdefe202f128dd2f464cc74639761f7a820d7586
yèct <yectocsalev@gmail.com>
2016-12-09 20:26:36 -0500

    ¡menta!

--- sketch.js
+++ sketch.js
@@ -1,18 +1,21 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	background(46, 240, 222);
 }
-
 
 function draw() {
-  background(0, 120, 0);
+  var tamango = random(11);
+  var ruidoX = 0.0002;
+  var ruidoY = 0.0001;
+  var posX = noise(millis() * ruidoX) * width - 20;
+  var posY = noise(millis() * ruidoY) * height - 20;
   noStroke();
-  fill(180, 0, 100, random(255));
-  background(255, 120, 0);
-  fill(180, 0, 100, random(25));
-  for (var i = 3; i < 2000; i += random(1000)) {
-    ellipse(width / 2, height / 2, i, i);
+  fill(255, random(255));
+  for (var x = 100; x < width; x += 4000) {
+    ellipse(posX, posY, tamango, tamango);
   }
 }
+
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
