bea2f4e83dfebfaaa44071de4af3f9086a828c41
yèct <yectocsalev@gmail.com>
2016-12-09 23:53:02 -0500

    merma

--- sketch.js
+++ sketch.js
@@ -7,12 +7,22 @@ function draw() {
   var tamango = random(11);
   var ruidoX = 0.0002;
   var ruidoY = 0.0001;
-  var posX = noise(millis() * ruidoX) * width - 20;
-  var posY = noise(millis() * ruidoY) * height - 20;
+  var posX = noise(millis() * ruidoX) * width - 200;
+  var posY = noise(millis() * ruidoY) * height - 200;
   noStroke();
   fill(255, random(255));
   for (var x = 100; x < width; x += 4000) {
     ellipse(posX, posY, tamango, tamango);
+  }
+  
+  var ruidoX1 = 0.0003;
+  var ruidoY1 = 0.0002;
+  var posX1 = noise(millis() * ruidoX1) * width - 200;
+  var posY1 = noise(millis() * ruidoY1) * height - 200;
+  noStroke();
+  fill(205, 0, 170, random(87));
+  for (var x = 10; x < width; x += 4000 * 33) {
+    ellipse(posX1, posY1, tamango, tamango);
   }
 }
 
