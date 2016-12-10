e1b5286b7f50ef0ececd5b0044f6e81dad0af552
yèct <yectocsalev@gmail.com>
2016-12-09 19:58:42 -0500

    ¡menta!

--- sketch.js
+++ sketch.js
@@ -4,13 +4,10 @@ function setup() {
 
 
 function draw() {
-<<<<<<< HEAD
   background(0, 120, 0);
   fill(180, 0, 100, random(255));
-=======
   background(255, 120, 0);
   fill(180, 0, 100, random(25));
->>>>>>> origin/master
   for (var i = 3; i < 2000; i += random(1000)) {
     ellipse(width / 2, height / 2, i, i);
   }
